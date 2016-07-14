9f7613340285a2146fe3eb1287056ee893703261
auroraguorui <auroraguorui@hotmail.com>
2016-07-13 22:49:50 -0700

    Create Sum_and_Count.js

---
+++ Sum_and_Count.js
@@ -0,0 +1,26 @@
+/* There are many ways to do this in MongoDB.
+
+count() is a cursor method that takes a query and returns a number equal to the amount of documents that matched the query.
+$sum is an aggregation operator availible in the $group stage, that can be used to both sum values and count the number of documents.
+mapReduce can produce a sum or a count during the results stage by using JavaScript.
+*/
+
+/* 1. .count() */
+db.world.count({"continent":"Africa"})
+
+/* 2. $sum */
+db.world.aggregate([
+   {"$group":{
+       "_id":"$continent",
+       "sum of populations":{$sum:"$population"},
+       "count of countries":{$sum:1}
+   }}
+
+])
+
+/* 3. Array.sum() */
+db.world.mapReduce(
+   function(){emit(this.continent, this.population)},
+   function(k,v){ return Array.sum(v) },
+   {out:{inline:1}}
+)

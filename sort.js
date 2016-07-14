e2442890a865cb5b21972371b8439b1064b645f8
auroraguorui <auroraguorui@hotmail.com>
2016-07-13 22:21:19 -0700

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,1 +1,3 @@
 /* 1. Sort all the documents in world by continent descending, then name ascending. Show only name and continent. */
+db.world.find({},{continent:1,name:1,_id:0}).sort({"continent":-1,"name":1})
+
